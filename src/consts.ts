e13026a11ea6361ecb0301c163096121d681093e
RT622 <127203903+RT622@users.noreply.github.com>
2026-01-27 04:30:58 +0800

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -9,7 +9,7 @@ export const BLOG_SLUG_KEY = process.env.BLOG_SLUG_KEY || ''
  */
 export const GITHUB_CONFIG = {
 	OWNER: process.env.NEXT_PUBLIC_GITHUB_OWNER || 'RT622',
-	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || 'blog',
+	REPO: process.env.NEXT_PUBLIC_GITHUB_REPO || '2gou',
 	BRANCH: process.env.NEXT_PUBLIC_GITHUB_BRANCH || 'main',
 	APP_ID: process.env.NEXT_PUBLIC_GITHUB_APP_ID || '2721846',
 	ENCRYPT_KEY: process.env.NEXT_PUBLIC_GITHUB_ENCRYPT_KEY || 'wudishiduomejimo',
